9541d687920652ab50ce6885e9d7ec8e87aee91b
Maylen <maylendeulofeu@gmail.com>
2022-06-23 19:37:15 -0400

    Create playerPrompt function for player's choices. Make choices case insensitive. Add conditional statements. Begin playerSelection function.

--- game.js
+++ game.js
@@ -9,5 +9,24 @@ function computerPlay() {
     return randomStrategy; // Return the resulting value.
 }
 
-let result = computerPlay(); // "Result" is a placeholder for when I figure out what variable to feed this value into.
-console.log(result); // "Show value in console so that I can see it."
+let computerSelection = computerPlay(); // "Result" is a placeholder for when I figure out what variable to feed this value into.
+console.log(computerSelection); // "Show value in console so that I can see it."
+
+function playerPrompt() { // Prompt for the player to type in their choice.
+    let playerChoice = (prompt('Choose rock, paper, or scissors.', '')); // playerChoice is the string entered into the prompt.
+        if (playerChoice.toLowerCase() === 'rock'.toLowerCase()) { // .toLowerCase() converts playerChoice to lowercase and compares to choices in lowercase, so that it is case insensitive.
+            return true // if playerChoice is true, it will pass on.
+        } else if (playerChoice.toLowerCase() === 'paper'.toLowerCase()) {
+            return true
+        } else if (playerChoice.toLowerCase() === 'scissors'.toLowerCase()) {
+            return true
+        } else {
+            console.log('Take the game seriously and try again.')
+        }
+
+    if (playerChoice === true) { 
+        return 
+    }
+}
+
+playerSelection();
