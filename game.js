6595b54d9ae2c8ec23adaab66c83b4f493258dd9
Maylen <maylendeulofeu@gmail.com>
2022-06-23 15:35:47 -0400

    Create computerPlay function

---
+++ game.js
@@ -0,0 +1,5 @@
+let greeting = "hello";
+function computerPlay(strategy) {  
+    let gameStrategies = ('Rock', 'Paper', 'Scissors');
+    let randomStrategy = gameStrategies(Math.floor(Math.random() * randomStrategy.length));
+}
